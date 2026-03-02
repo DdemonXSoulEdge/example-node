8756a58eff7f5a504548912567c9f4a2bac42925
PEmilio <palomillo.flores@gmail.com>
2026-03-02 16:31:13 -0600

    Add files via upload

--- load-test.js
+++ load-test.js
@@ -3,29 +3,30 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 20 },  // Ramp up
-    { duration: '1m', target: 20 },   // Stay at 20 users
-    { duration: '30s', target: 0 },   // Ramp down
+    { duration: '20s', target: 10 },  // Ramp up
+    { duration: '40s', target: 10 },  // Steady load
+    { duration: '20s', target: 0 },   // Ramp down
   ],
   thresholds: {
-    http_req_duration: ['p(95)<500'], // 95% < 500ms
-    http_req_failed: ['rate<0.01'],   // <1% errors
+    http_req_duration: ['p(95)<2000'], // 95% under 2s
+    http_req_failed: ['rate<0.05'],    // <5% errors
   },
 };
 
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://fast-api-dev.onrender.com';
 
 export default function () {
+
   let usersRes = http.get(`${BASE_URL}/users`);
   check(usersRes, {
-    'Users status 200': (r) => r.status === 200,
+    'Users status is 200': (r) => r.status === 200,
   });
 
   sleep(1);
 
   let itemsRes = http.get(`${BASE_URL}/items`);
   check(itemsRes, {
-    'Items status 200': (r) => r.status === 200,
+    'Items status is 200': (r) => r.status === 200,
   });
 
   sleep(1);
